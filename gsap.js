6ee937b78d41f0627aeede745d3c0b5a91cff656
= <=>
2021-09-03 01:05:57 +0530

    added gsap animations for side menu and added gsap's scrollTo addon for page motions

--- gsap.js
+++ gsap.js
@@ -2,29 +2,58 @@
 gsap.from("main",{
     duration :3,
     opacity : 0,
-    skewY : "10deg",
+    skewY : "-10deg",
     x:"100px",
     y:"-50px",
     delay: 2,
-    ease : "expo.inOut"
+    ease : "power4.inOut"
 })
 
 
+
 let menuBtn = document.querySelector('#navLink');
-menuBtn.addEventListener('click',function(e){
-    e.preventDefault();
-    let tl = gsap.timeline({});
-    tl.to('#menu-f1',{
-        duration: 3,
+let menuCloseBtn = document.querySelector('#menuCloseBtn');
+let aboutMeBtn = document.querySelector('#scrollTo__aboutMe');
+let contactBtn = document.querySelector('#scrollTo__contact');
+let workBtn = document.querySelector('#scrollTo__work');
+let backToTopBtn = document.querySelector('#scroll__backToTop');
+
+let menuTimeline = gsap.timeline({});
+
+function toggleMenuClass(){
+    if (menuTimeline.reversed()) {
+        menuTimeline.play();
+        return;
+    }
+    menuBtn.classList.toggle('menuOpen');
+    
+    menuTimeline.fromTo('#menu-f1',{
+        x:"-100%",
+        y:"100%",
+        borderRadius:"80%",
+        // skewY:"50deg",
+    },{
+        duration: 2,
         ease: 'expo.inOut',
+        borderRadius:"0%",
+        // skewY:"0deg",
         x:0,
+        y:0,
     })
-    tl.to('#menu-f2',{
-        duration: 3.5,
+    menuTimeline.fromTo('#menu-f2',{
+        x:"-100%",
+        y:"100%",
+        borderRadius:"80%",
+        // skewY:"50deg",
+    },{
+        duration: 2,
         ease: 'expo.inOut',
+        borderRadius:"0%",
+        // skewY:"0deg",
         x:0,
-    },"-=2.6")
-    tl.fromTo('#menu',{
+        y:0,
+    },'-=1.9')
+    menuTimeline.fromTo('#menu',{
         x:"-100%",
         y:"100%",
         borderRadius:"80%",
@@ -36,6 +65,60 @@ menuBtn.addEventListener('click',function(e){
         // skewY:"0deg",
         x:0,
         y:0,
-    },'-=2.8')
+    },'-=2.2')
 
+    menuTimeline.fromTo('.menuLink',{
+        y:50,
+        opacity:0,
+        skewY:"-10deg",
+        visibility:'hidden'
+    },{
+        duration:1,
+        ease:'expo.inOut',
+        y:0,
+        skewY:"0deg",
+        opacity:1,
+        visibility:'visible',
+        stagger:0.2
+    },"-=1.2")
+}
+
+
+menuBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    toggleMenuClass();
+})
+
+
+menuCloseBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    menuTimeline.reverse();
+    menuBtn.classList.toggle('menuOpen');
+
+})
+
+aboutMeBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    menuTimeline.reverse();
+    menuBtn.classList.toggle('menuOpen');
+
+    gsap.to(window,{duration:3,scrollTo:'#me',ease:'expo.inOut'});
+});
+workBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    menuTimeline.reverse();
+    menuBtn.classList.toggle('menuOpen');
+
+    gsap.to(window,{duration:3,scrollTo:'#work',ease:'expo.inOut'});
+})
+contactBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    menuTimeline.reverse();
+    menuBtn.classList.toggle('menuOpen');
+
+    gsap.to(window,{duration:3,scrollTo:'#contact',ease:'expo.inOut'});
+})
+backToTopBtn.addEventListener('click',function(e){
+    e.preventDefault();
+    gsap.to(window,{duration:3,scrollTo:'#topNavigation',ease:'expo.inOut'});
 })
